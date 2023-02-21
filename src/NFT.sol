ca066beb44f648b35465483f14e7a092f0255d3c
Shanmugadevan <shanmugadevan@protonmail.com>
2023-02-21 09:15:10 +0530

    Set up entry point and Aawallet files

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-upgrades/packages/core/contracts/";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC1155/ERC1155Upgradeable.sol";
+
+contract NFT is ERC1155Upgradeable {
+    
+}
